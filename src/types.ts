a010bf1668e1dc2119f056353649af74eea98cb0
Benjamin Jesuiter <benjamin.jesuiter.extern@atruvia.de>
2023-06-08 15:34:08 +0200

    move zod types in extra file

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const BinaryConfig = z.object({
+  path: z.string().min(1),
+  platform: z.string().min(1),
+  arch: z.string().min(1),
+});
+
+export type BinaryConfig = z.infer<typeof BinaryConfig>;
+
+export const Bin2NpmConfig = z.object({
+  version: z.string().min(1),
+  binaries: z.array(BinaryConfig),
+});
+
+export type Bin2NpmConfig = z.infer<typeof Bin2NpmConfig>;
