1a34164a32b57998da1c9ccb6bd6c86d55b0b208
Benjamin Jesuiter <benjamin.jesuiter.extern@atruvia.de>
2023-06-09 10:49:45 +0200

    finish rendering template for package json

---
+++ src/builder/package.json.template.ts
@@ -0,0 +1,69 @@
+import { join } from "std/path/mod.ts";
+import { TargetPackageJson } from "../types.ts";
+
+const packageJson = {
+  // name, version and description fields will be added from the bin2npm config!
+  // 'bin' will also be added from bin2npm config
+  // bin: {
+  //   "binary-name": "./bin/entrypoint.mjs",
+  // },
+  name: "",
+  version: "",
+  description: "",
+  bin: {},
+  type: "module",
+  // Not needded for type module
+  // main: "index.js",
+  files: [
+    // Note: Automatically added files:
+    // main script (if applicable), Readme.md, package.json
+    "bin",
+    "Readme.md",
+    "package.json",
+  ],
+  scripts: {
+    start: "node ./bin/entrypoint.mjs",
+  },
+  dependencies: {
+    execa: "^7.1.1",
+  },
+  devDependencies: {
+    "@types/node": "^18.11.9",
+  },
+  // Note: Not in use right now
+  // repository: {
+  //   type: "git",
+  //   url: "git+https://github.com/codemonument/deno-bin-offline.git",
+  // },
+  // keywords: ["deno"],
+  // author: "Benjamin Jesuiter",
+  // license: "MIT",
+  // bugs: {
+  //   url: "https://github.com/codemonument/deno-bin-offline/issues",
+  // },
+  // homepage: "https://github.com/codemonument/deno-bin-offline#readme",
+};
+
+export async function renderPackageJson(
+  config: TargetPackageJson,
+  outPath?: string
+) {
+  if (!outPath) {
+    outPath = `dist/`;
+  }
+
+  packageJson.name = config.name;
+  packageJson.version = config.version;
+  packageJson.description = config.description;
+  const binEntries =
+    config.binAliases?.map((alias) => [alias, "./bin/entrypoint.mjs"]) ?? [];
+  binEntries.push([config.name, "./bin/entrypoint.mjs"]);
+  packageJson.bin = Object.fromEntries(binEntries);
+
+  await Deno.writeTextFile(
+    join(outPath, "package.json"),
+    JSON.stringify(packageJson, null, "\t")
+  );
+
+  console.info(`Rendered package.json!`);
+}
