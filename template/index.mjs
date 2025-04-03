8030b2e62256ca52e0b85c578389270f3cf688aa
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-04-03 12:37:40 +0200

    update error message in template

--- template/index.mjs
+++ template/index.mjs
@@ -1,3 +1,3 @@
 throw new Error(
-  "This module shouldn't be required or imported by other modules. Use this module as a CLI."
+	"This package shouldn't be required or imported by other packages. This package only exposes one or more CLI tools."
 );
